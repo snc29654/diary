b3a0b1286ac2cbcef40ccff51949d61cce0048f3
snc29654 <snc29654@nifty.com>
2025-02-20 09:49:10 +0900

    リンク情報にa hrefを付加する

--- diary_memo.py
+++ diary_memo.py
@@ -66,7 +66,7 @@ def copy_link(url):
 
     text = ""
     for result in results:
-        text += result["text"] + "\t" + result["url"] + "\n"   
+        text +=  "<a href= \"" + result["url"] + "\"</a>"+result["text"] + "\t" +"<br>" +"\n"   
     return(text)
 
 def  inet_data_print(match_word):
